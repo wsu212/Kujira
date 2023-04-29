05e1aae2e12ac4e29a82a514d1121e6a925142a6
wsu212 <wsu212@gmail.com>
2023-04-29 18:56:33 -0400

    Initial Commit

---
+++ Kujira/KujiraApp.swift
@@ -0,0 +1,17 @@
+//
+//  KujiraApp.swift
+//  Kujira
+//
+//  Created by 蘇偉綸 on 2023/4/29.
+//
+
+import SwiftUI
+
+@main
+struct KujiraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
